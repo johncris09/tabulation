48c95727ee747ae02dab686b3d6f79e7d42c7edc
John Cris <johncrismanabo9@gmail.com>
2022-09-12 14:46:30 +0800

    assets

---
+++ assets/js/login.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+    // Log in User
+    $(document).on('submit','#formAuthentication',function(e){  
+      e.preventDefault();  
+      $.ajax({
+        url: BASE_URL + 'login/login',
+        type: 'POST',  
+        data: $('#formAuthentication').serialize(),
+        dataType: 'JSON',
+        success: function(data){ 
+            if(data.response){
+                location.reload();
+            }else{ 
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'Invalid Username/Password', 
+                })
+            }
+        },
+        // Error Handler
+        error: function(xhr, textStatus, error){
+          console.info(xhr.responseText);
+        }
+      }); 
+  
+    }); 
+  
+});
