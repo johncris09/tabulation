41bbed0c58a775ca86685a3be6f047fbb96aaeab
John Cris <johncrismanabo9@gmail.com>
2022-09-12 15:32:23 +0800

    swim wear

---
+++ application/models/Swim_wear_model.php
@@ -0,0 +1,139 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Swim_wear_model extends CI_Model
+{
+
+
+	public $table = 'swim_wear';
+	
+    public function __construct()
+    {
+        parent::__construct();
+    } 
+
+    public function insert($data)
+    {
+        
+        $insert = $this->db->insert($this->table, $data);
+        if(!$insert && $this->db->error()['code'] == 1062){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    
+    public function update($data)
+    { 
+        return $this->db
+            ->where('id', $data['id'])
+            ->update($this->table, $data);
+    }
+
+
+
+
+    public function if_exist($data)
+    {   
+        $this->db->where('candidate', $data['candidate']);
+        $this->db->where('judge', $data['judge']);
+        return $this->db->get($this->table); 
+    } 
+
+    function get_candidate_score($data)
+    { 
+        $this->db->where($data);
+        return $this->db->get($this->table)->result_array()[0]['score']; 
+
+    }
+
+    function get_candidate_consolidated_score($data)
+    {
+        $this->db->where($data);
+        $this->db->where('user.id = swim_wear.judge');
+        return $this->db->get('user, swim_wear');//->result_array()[0]['score']; 
+    }
+
+    
+    function get_tot_score($data)
+    {  
+        $this->db->select('swim_wear.candidate, sum(rank) as tot_score');
+        $this->db->where($data); 
+        $this->db->where('swim_wear.judge != 0');
+        return $this->db->get('swim_wear');//->result_array()[0]['score']; 
+    }
+
+    
+
+
+
+    
+    
+    
+    function get_all_score()
+    { 
+        return $this->db->get($this->table)->result_array()[0]['score']; 
+    }
+
+    
+
+    public function get_user($data)
+    {  
+        $this->db->where($data);
+        return $this->db->get($this->table)->result_array()[0];
+    } 
+  
+
+    public function delete($id)
+    {
+        return $this->db
+            ->where('id', $id)
+            ->delete($this->table);
+    }
+
+    function get_candidate_rank_consolidate($data)
+    {
+        $this->db->where($data);
+        $this->db->where("user.id =  swim_wear.judge");
+        return $this->db->get('swim_wear, user');
+    }
+    
+    function get_judge()
+    {
+        
+        $this->db->select(' DISTINCT(judge)');  
+        $this->db->from('swim_wear, user');  
+        $this->db->where('swim_wear.judge', 'user.id');  
+        $this->db->where('user.role_type', 'judge');  
+        return $this->db->get_compiled_select();
+
+        
+    }
+
+    function get_ranking_specific_judge($data)
+    {
+        return $this->db
+            ->where($data)
+            ->order_by('score', 'DESC')
+            ->get($this->table)->result_array();
+    }
+
+    function get_consolidated_ranking($data)
+    {
+        return $this->db
+            ->where('score != 0')
+            ->where($data)
+            ->order_by('score', 'asc')
+            ->get($this->table)->result_array();
+    }
+
+    function update_rank($data)
+    {
+        return $this->db
+            ->where('candidate', $data['candidate'])
+            ->where('judge', $data['judge']) 
+            ->update($this->table, $data);
+    }
+ 
+}
---
+++ application/views/user/swim_wear.php
@@ -0,0 +1,223 @@
+<!DOCTYPE html> 
+<html lang="en" class="light-style " dir="ltr" data-theme="theme-default" data-assets-path="<?php echo base_url(); ?>assets/" data-template="horizontal-menu-template">
+
+<head>
+	<?php $this->view('layout/meta') ?>
+	<?php $this->view('layout/css') ?>
+  <style>
+    .star {
+      font-weight: bolder;
+      height: 40px;
+      width: 40px;
+      -webkit-clip-path: polygon(50% 0%, 61% 35%, 98% 35%, 68% 57%, 79% 91%, 50% 70%, 21% 91%, 32% 57%, 2% 35%, 39% 35%);
+      clip-path: polygon(50% 0%, 61% 35%, 98% 35%, 68% 57%, 79% 91%, 50% 70%, 21% 91%, 32% 57%, 2% 35%, 39% 35%);
+      text-align: center;
+      background: #F0D512;
+      color: white;
+      float: left; 
+      color: black;
+    }
+
+    .star::before {
+      display: inline-block;
+      height: 100%;
+      background: blue;
+      vertical-align: middle;
+      content: '';
+    }
+
+    input[type="number"]::-webkit-outer-spin-button,
+    input[type="number"]::-webkit-inner-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+    }
+    input[type="number"] {
+        -moz-appearance: textfield;
+    }
+
+
+
+  </style>
+</head>
+
+<body> 
+	<div class="layout-wrapper layout-navbar-full layout-horizontal layout-without-menu">
+		<div class="layout-container">
+			<?php $this->view('layout/header') ?> 
+				<div class="layout-page"> 
+					<div class="content-wrapper">
+						<?php $this->view('layout/menu') ?>
+						<div class="container-xxl flex-grow-1 container-p-y">
+            
+        
+
+            <!-- Basic Bootstrap Table -->
+            <div class="card"> 
+              <div class="card-header d-flex justify-content-between pb-3">
+                <div class="conversion-title">
+                  <h5 class="card-title mb-1"> <?php echo $page_title; ?> </h5>
+                  
+                </div>
+                <button class="btn btn-primary" id="submit-score"> <i class=" bx bx-check"> </i> Submit Score</button>
+              </div>
+              <div class="table-responsive text-nowrap">
+                <table class="table">
+                  <thead>
+                    <tr> 
+                      <th>Candidate</th> 
+                      <th>Score</th>
+                      <th>Rank</th> 
+                    </tr>
+                  </thead>
+                  <tbody class="table-border-bottom-0">
+                    <?php 
+                      if($candidate->num_rows() > 0){
+                        foreach($candidate->result_array() as $row){
+                          echo '
+                            <tr> 
+                              <td> <div class="star">' .$row['number']. '</div>   <div style="margin-top: 10px !important;">' .$row['name'].'</div></td> 
+                              <td><input type="number" data-candidate="'.$row['id'].'"  step="0.01" min="1" max="10" onKeyUp="if(this.value>10){this.value=\'10\';}else if(this.value<0){this.value=\'0\';}" class="form-control text-center candidate"></td>
+                              <td> <span data-candidate="'.$row['id'].'" class="rank h6 text-center candidate-'.$row['id'].'"></span> </td> 
+                            </tr>
+                          ';
+                        }
+
+                      }
+                    ?> 
+                  </tbody>
+                </table>
+              </div>
+            </div> 
+            
+
+          </div>
+  
+
+						<?php $this->view('layout/footer') ?>
+						<div class="content-backdrop fade"></div>
+					</div> 
+				</div> 
+		</div>
+	</div> 
+	<div class="layout-overlay layout-menu-toggle"></div> 
+	<div class="drag-target"></div> 
+	<?php $this->view('layout/js') ?>
+  <script>
+    // window.onload = function () {
+    //   load_rank();
+    // }
+    $(document).ready(function(){  
+      load_score(); 
+      load_rank();
+      
+
+    function load_rank(){
+ 
+      $.ajax({
+        url: BASE_URL + 'swim_wear/get_ranking_specific_judge',
+        type: 'POST',   
+        dataType: 'JSON',
+        success: function(data){  
+          // console.info(data)
+          $.each(data, function (key, val) {  
+            $('span.candidate-' + val.candidate).html(val.rank) 
+          });
+          
+        }, 
+        error: function(xhr, textStatus, error){
+          console.info(xhr.responseText);
+        }
+      }); 
+
+    }
+
+      // load score
+    function load_score()
+      {
+        $('input.candidate').each(function(){   
+          var _this = $(this)
+            $.ajax({
+              type : 'POST',
+              url : BASE_URL + "swim_wear/get_candidate_score",
+              data : { 
+                candidate : $(this).data('candidate'),
+                judge : '<?php echo $_SESSION['id'] ?>', 
+              },
+              dataType: "json",
+              success : function(data){   
+                $(_this).val(data)
+              }
+            }); 
+        })
+      }
+      // save score while typing
+      $('input').on('keyup', function(){  
+        var _this = $(this)  
+        $.ajax({
+          type : 'POST',
+          url : BASE_URL + "swim_wear/insert", 
+          data : {
+            score : $(this).val(),
+            candidate : $(this).data('candidate'),
+            judge : '<?php echo $_SESSION['id'] ?>',
+
+          },
+          dataType: "json",
+          success : function(data){   
+            if(data.response == true){ 
+              
+              $(_this).css("border", "2px solid blue");
+              $(_this).css('font-weight', 'bolder');
+              setTimeout(function() {  
+                $(_this).css("border", "1px solid black");
+                $(_this).css('font-weight', 'normal');
+              }, 500); 
+              
+            }
+
+            load_rank();
+          }
+        });
+      });
+      
+
+      // submit score 
+      $('#submit-score').on('click', function(){  
+        var _this = $(this)  
+        $('.rank').each(function(){   
+          var _this = $(this) 
+          $.ajax({
+            type : 'POST',
+            url : BASE_URL + "swim_wear/update_rank",
+            data : { 
+              candidate : $(this).data('candidate'),
+              judge : '<?php echo $_SESSION['id'] ?>', 
+              rank : $(this).html(),
+            },
+            dataType: "json",
+            success : function(data){  
+            }, 
+            error: function(xhr, textStatus, error){
+              console.info(xhr.responseText);
+            }
+          }); 
+        })
+  
+        Swal.fire({
+          icon: 'success',
+          title: 'Score Submitted', 
+        }) 
+         
+      });
+
+
+
+ 
+      });
+
+
+
+  </script>
+</body>
+
+</html>
