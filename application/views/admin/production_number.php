4f833c8cd657fab3ffe24028f48b12c67067b98a
John Cris <johncrismanabo9@gmail.com>
2022-09-12 14:49:17 +0800

    set interval

--- application/views/admin/production_number.php
+++ application/views/admin/production_number.php
@@ -44,64 +44,64 @@
 						<?php $this->view('layout/menu') ?>
 						<div class="container-xxl flex-grow-1 container-p-y">
             
-        
+                    
 
-<!-- Basic Bootstrap Table -->
-<div class="card"> 
-  <div class="card-header d-flex justify-content-between pb-3">
-    <div class="conversion-title">
-      <h5 class="card-title mb-1"> <?php echo $page_title; ?> </h5> 
-    </div> 
-  </div>
-  <div class="table-responsive text-nowrap">
-    <table class="table">
-      <thead>
-        <tr> 
-          <th>Candidate</th> 
-          <?php
-            foreach($judge as $row){
-              echo '
-                <th>'.$row['name'].'</th>
-              ';
-            } 
-          ?>
-          <th>Total Score</th> 
-          <th>Rank</th> 
-        </tr>
-      </thead>
-      <tbody class="table-border-bottom-0">
-        <?php 
-        
-        $tot_score = 0;
-          if($candidate->num_rows() > 0){
-            foreach($candidate->result_array() as $row){
-              echo '
-                <tr class="candidate-row">
-                  <td> <div class="star">' .$row['number']. '</div>   <div style="margin-top: 10px !important;">' .$row['name'].'</div></td>   
-                  <td class="candidate-consolidate judge1 candidate-'.$row['id'].'" data-candidate="'.$row['id'].'" data-judge="judge1"></td>   
-                  <td class="candidate-consolidate judge2 candidate-'.$row['id'].'" data-candidate="'.$row['id'].'" data-judge="judge2"></td>  
-                  <td class="candidate-consolidate judge3 candidate-'.$row['id'].'" data-candidate="'.$row['id'].'" data-judge="judge3"></td>  
-                  <td class="candidate-consolidate judge4 candidate-'.$row['id'].'" data-candidate="'.$row['id'].'" data-judge="judge4"></td>  
-                  <td class="candidate-consolidate judge5 candidate-'.$row['id'].'" data-candidate="'.$row['id'].'" data-judge="judge5"></td>  
-                ';  
-              echo '    
-                  <td data-candidate="'.$row['id'].'"  data-score="'.$tot_score.'" class="text-center candidate-'.$row['id'].' tot-score" style="border-left: 1px dotted #677788;border-right: 1px dotted #677788;"></td>  
-                  <td class="text-center"> <span data-candidate="'.$row['id'].'" class="rank h6 text-center candidate-'.$row['id'].'"></span></td>  
-                </tr>
-              ';
-              
-              $tot_score = 0;
+            <!-- Basic Bootstrap Table -->
+            <div class="card"> 
+              <div class="card-header d-flex justify-content-between pb-3">
+                <div class="conversion-title">
+                  <h5 class="card-title mb-1"> <?php echo $page_title; ?> </h5> 
+                </div> 
+              </div>
+              <div class="table-responsive text-nowrap">
+                <table class="table">
+                  <thead>
+                    <tr> 
+                      <th>Candidate</th> 
+                      <?php
+                        foreach($judge as $row){
+                          echo '
+                            <th>'.$row['name'].'</th>
+                          ';
+                        } 
+                      ?>
+                      <th>Total Score</th> 
+                      <th>Rank</th> 
+                    </tr>
+                  </thead>
+                  <tbody class="table-border-bottom-0">
+                    <?php 
+                    
+                    $tot_score = 0;
+                      if($candidate->num_rows() > 0){
+                        foreach($candidate->result_array() as $row){
+                          echo '
+                            <tr class="candidate-row">
+                              <td> <div class="star">' .$row['number']. '</div>   <div style="margin-top: 10px !important;">' .$row['name'].'</div></td>   
+                              <td class="candidate-consolidate judge1 candidate-'.$row['id'].'" data-candidate="'.$row['id'].'" data-judge="judge1"></td>   
+                              <td class="candidate-consolidate judge2 candidate-'.$row['id'].'" data-candidate="'.$row['id'].'" data-judge="judge2"></td>  
+                              <td class="candidate-consolidate judge3 candidate-'.$row['id'].'" data-candidate="'.$row['id'].'" data-judge="judge3"></td>  
+                              <td class="candidate-consolidate judge4 candidate-'.$row['id'].'" data-candidate="'.$row['id'].'" data-judge="judge4"></td>  
+                              <td class="candidate-consolidate judge5 candidate-'.$row['id'].'" data-candidate="'.$row['id'].'" data-judge="judge5"></td>  
+                            ';  
+                          echo '    
+                              <td data-candidate="'.$row['id'].'"  data-score="'.$tot_score.'" class="text-center candidate-'.$row['id'].' tot-score" style="border-left: 1px dotted #677788;border-right: 1px dotted #677788;"></td>  
+                              <td class="text-center"> <span data-candidate="'.$row['id'].'" class="rank h6 text-center candidate-'.$row['id'].'"></span></td>  
+                            </tr>
+                          ';
+                          
+                          $tot_score = 0;
 
-            }
+                        }
 
 
-          }
-        ?> 
-      </tbody>
-    </table>
-  </div>
-</div> 
-            
+                      }
+                    ?> 
+                  </tbody>
+                </table>
+              </div>
+            </div> 
+                        
 
           </div>
   
@@ -119,11 +119,11 @@
     
     $(document).ready(function(){  
       
-      // setInterval(function(){  
+      setInterval(function(){  
         consolidate_rank()  
         get_tot_score();
         load_rank();
-      // }, 500); 
+      }, 1000); 
       
  
       function consolidate_rank(){
@@ -137,7 +137,7 @@
               judge_no : $(this).data('judge'), 
             },
             dataType: "json",
-            success : function(data){    
+            success : function(data){  
               $('td.candidate-consolidate.'+data.judge_no+'.candidate-' + data.candidate).html(data.rank == 0 ? "" : data.rank) 
               
 
@@ -159,7 +159,7 @@
               candidate : $(this).data('candidate'), 
             },
             dataType: "json",
-            success : function(data){    
+            success : function(data){   
               $('td.candidate-' + data.candidate +'.tot-score').html(data.tot_score == 0 ? "" : data.tot_score) 
               $.ajax({
                 type : 'POST',
@@ -170,8 +170,7 @@
                   judge : 0, 
                 },
                 dataType: "json",
-                success : function(data){  
-                  
+                success : function(data){ 
                 }
               });
 
@@ -183,37 +182,6 @@
         })
       } 
       
-      // submit score 
-      // $('#score').on('click', function(){  
-      //   var _this = $(this)   
-      //   $('td.candidate').each(function(){   
-      //     var _this = $(this)  
-      //     $.ajax({
-      //       type : 'POST',
-      //       url : BASE_URL + "production_number/insert_consolidated_score",
-      //       data : { 
-      //         candidate : $(this).data('candidate'), 
-      //         score : $(this).data('score'), 
-      //         judge : 0, // set 0 to admin  
-      //       },
-      //       dataType: "json",
-      //       success : function(data){  
-      //         console.info(data)
-      //       }, 
-      //       error: function(xhr, textStatus, error){
-      //         console.info(xhr.responseText);
-      //       }
-      //     }); 
-      //   }) 
-
-        
-      //   Swal.fire({
-      //     icon: 'success',
-      //     title: 'Score Saved!', 
-      //   })  
-      //   load_rank(); 
-         
-      // });  
 
 
       function load_rank(){ 
@@ -221,12 +189,9 @@
           url: BASE_URL + 'production_number/get_consolidated_ranking',
           type: 'POST',   
           dataType: 'JSON',
-          success: function(data){  
-            console.info(data) 
+          success: function(data){   
             $.each(data, function (key, val) {    
-              $('span.rank.candidate-' + val.candidate).html(val.rank) 
-              
-
+              $('span.rank.candidate-' + val.candidate).html(val.rank)  
               // update rank
               $.ajax({
                 type : 'POST',
