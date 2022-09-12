255e87a8fc9d7ae3d27cb021a0a27ca9970ebeed
John Cris <johncrismanabo9@gmail.com>
2022-09-12 15:33:05 +0800

    rename page title

--- application/views/login.php
+++ application/views/login.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
-    <title>Login Cover - Pages | Frest - Bootstrap Admin Template</title>
+    <title><?php echo $page_title; ?></title>
     
     <meta name="description" content="Start your development with a Dashboard for Bootstrap 5" />
     <meta name="keywords" content="dashboard, bootstrap 5 dashboard, bootstrap 5 admin, bootstrap 5 design, bootstrap 5">
