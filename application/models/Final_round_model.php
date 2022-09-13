0a4192ed36de4dda49b68d464ad0d5e5e3050489
John Cris <johncrismanabo9@gmail.com>
2022-09-13 10:57:35 +0800

    Final Round

---
+++ application/models/Final_round_model.php
@@ -0,0 +1,158 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Final_round_model extends CI_Model
+{
+
+
+	public $table = 'final_round123';
+	
+    public function __construct()
+    {
+        parent::__construct();
+    } 
+
+    public function is_exist($data)
+    {    
+        $this->db->where($data);
+        return $this->db->get('final_round'); 
+    } 
+
+
+    public function insert($data)
+    {
+        
+        $insert = $this->db->insert('final_round', $data);
+        if(!$insert && $this->db->error()['code'] == 1062){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    
+    public function get_all_candidate()
+    { 
+        return $this->db
+            ->select('distinct(final_round.candidate), candidate.*')
+            ->where('candidate.id = final_round.candidate')
+            ->order_by('candidate.number')
+			->get('final_round, candidate');
+    }
+
+
+
+
+    
+    public function update($data)
+    { 
+        return $this->db
+            ->where('id', $data['id'])
+            ->update('final_round', $data);
+    }
+
+
+    public function if_exist($data)
+    {   
+        $this->db->where('candidate', $data['candidate']);
+        $this->db->where('judge', $data['judge']);
+        return $this->db->get('final_round'); 
+    } 
+
+     
+    function get_candidate_score($data)
+    { 
+        $this->db->where($data);
+        return $this->db->get('final_round')->result_array()[0]['score']; 
+
+    }
+
+    function get_candidate_consolidated_score($data)
+    {
+        $this->db->where($data);
+        $this->db->where('user.id = final_round.judge');
+        return $this->db->get('user, final_round');//->result_array()[0]['score']; 
+    }
+
+    
+    function get_tot_score($data)
+    {  
+        $this->db->select('final_round.candidate, sum(rank) as tot_score');
+        $this->db->where($data); 
+        $this->db->where('final_round.judge != 0');
+        return $this->db->get('final_round');//->result_array()[0]['score']; 
+    }
+
+    
+
+
+
+    
+    
+    
+    // function get_all_score()
+    // { 
+    //     return $this->db->get('final_round')->result_array()[0]['score']; 
+    // }
+
+    
+
+    // public function get_user($data)
+    // {  
+    //     $this->db->where($data);
+    //     return $this->db->get('final_round')->result_array()[0];
+    // } 
+  
+
+    // public function delete($id)
+    // {
+    //     return $this->db
+    //         ->where('id', $id)
+    //         ->delete('final_round');
+    // }
+
+    // function get_candidate_rank_consolidate($data)
+    // {
+    //     $this->db->where($data);
+    //     $this->db->where("user.id =  final_round.judge");
+    //     return $this->db->get('final_round, user');
+    // }
+    
+    // function get_judge()
+    // {
+        
+    //     $this->db->select(' DISTINCT(judge)');  
+    //     $this->db->from('final_round, user');  
+    //     $this->db->where('final_round.judge', 'user.id');  
+    //     $this->db->where('user.role_type', 'judge');  
+    //     return $this->db->get_compiled_select();
+
+        
+    // }
+
+    function get_ranking_specific_judge($data)
+    {
+        return $this->db
+            ->where($data)
+            ->order_by('score', 'DESC')
+            ->get('final_round')->result_array();
+    }
+
+    function get_consolidated_ranking($data)
+    {
+        return $this->db
+            ->where('score != 0')
+            ->where($data)
+            ->order_by('score', 'asc')
+            ->get('final_round')->result_array();
+    }
+
+    function update_rank($data)
+    {
+        return $this->db
+            ->where('candidate', $data['candidate'])
+            ->where('judge', $data['judge']) 
+            ->update('final_round', $data);
+    }
+ 
+}
