8ecbde56c07dd8394da164acbfdf6043f53f7e86
John Cris <johncrismanabo9@gmail.com>
2022-09-16 08:22:54 +0800

    change redirect

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -22,8 +22,8 @@
       
       
       <div class="layout-menu-toggle navbar-nav align-items-xl-center me-3 me-xl-0  d-xl-none  ">
-        <a class="nav-item nav-link px-0 me-xl-4" href="javascript:void(0)">
-          <i class="bx bx-menu bx-sm"></i>
+        <a class="nav-item nav-link px-0 me-xl-4" href="<?php echo base_url(); ?>">
+          <i class="bx bx-menu bx-sm"></i> 
         </a>
       </div>
       
