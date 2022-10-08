6d033550371dced19231d32b7a9b558d42db3501
John Cris <johncrismanabo9@gmail.com>
2022-10-08 14:09:25 +0800

    one account per device only

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -27,12 +27,34 @@ class Login extends CI_Controller {
 
 		$login = $this->user_model->login($user);  
 
-		if($login){
-			$this->set_session($user);
-			$data['response'] = true;
-		}else{
-			$data['response'] = false;
-		}
+        // 1 account per device only
+        if($login->num_rows() > 0){ 
+            $is_logged_in = $login->result_array()[0]['is_logged_in'];
+            if($is_logged_in){  
+                $data = array(
+                    'response' => false,
+                    'message' => "One account per device only",
+                );
+            }else{
+
+                $user_id  = $this->user_model->get_user($user)['id']; 
+                $is_logged_in_update = array(
+                    'is_logged_in' => 1,
+                    'id' => $user_id
+                );
+
+                $update = $this->user_model->update($is_logged_in_update);
+
+                $this->set_session($user);
+                $data['response'] = true;
+            }
+
+        }else{
+            $data = array(
+                'response' => false
+            );
+        }
+ 
 
 		echo json_encode($data);
 		 
