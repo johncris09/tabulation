f1658c583f05edb617dc2208f1a65434feeec20c
John Cris <johncrismanabo9@gmail.com>
2022-10-08 14:02:33 +0800

    set interval in bubble effect

--- application/views/login.php
+++ application/views/login.php
@@ -189,72 +189,85 @@
   <!-- <script src="<?php echo base_url(); ?>assets/js/pages-auth.js"></script> -->
 	<script src="http://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script> 
 	<script src="http://threejs.org/examples/js/libs/stats.min.js"></script>
-	<script>
-		particlesJS("particles-js", {
-  particles: {
-    number: {
-      value: 20,
-      density: { enable: true, value_area: 1603.4120608655228 }
-    },
-    color: { value: "#ffffff" },
-    shape: {
-      type: "circle",
-      stroke: { width: 0, color: "#fff" },
-      polygon: { nb_sides: 6 },
-      image: { src: "<?php echo base_url() ?>assets/img/bubble.jpg", width: 100, height: 100 }
-    },
-    opacity: {
-      value: 0.5,
-      random: true,
-      anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false }
-    },
-    size: {
-      value: 95,
-      random: true,
-      anim: {
-        enable: true,
-        speed: 0,
-        size_min: 0,
-        sync: false
-      }
-    },
-    line_linked: {
-      enable: false,
-      distance: 978.5430756240181,
-      color: "#ffffff",
-      opacity: 0.3,
-      width: 2.8409315098761816
-    },
-    move: {
-      enable: true,
-      speed: 20,
-      direction: "none",
-      random: true,
-      straight: false,
-      out_mode: "out",
-      bounce: false,
-      attract: { enable: true, rotateX: 600, rotateY: 1183.721462448409 }
-    }
-  },
-  interactivity: {
-    detect_on: "canvas",
-    events: {
-      onhover: { enable: false, mode: "grab" },
-      onclick: { enable: false, mode: "push" },
-      resize: true
-    },
-    modes: {
-      grab: { distance: 400, line_linked: { opacity: 1 } },
-      bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
-      repulse: { distance: 200, duration: 0.4 },
-      push: { particles_nb: 4 },
-      remove: { particles_nb: 2 }
-    }
-  },
-  retina_detect: true
-});
-requestAnimationFrame(update);
+	<script> 
+  
 
+    $(document).ready(function() {
+      
+      
+      setInterval(function() {
+        bubble()
+      }, Math.floor(Math.random() * (10000 - 3000)) + 3000 ); 
+
+      function bubble(){
+        particlesJS("particles-js", {
+          particles: {
+            number: {
+              value: 20,
+              density: { enable: true, value_area: 1603.4120608655228 }
+            },
+            color: { value: "#ffffff" },
+            shape: {
+              type: "circle",
+              stroke: { width: 0, color: "#fff" },
+              polygon: { nb_sides: 6 },
+              image: { src: "<?php echo base_url() ?>assets/img/bubble.jpg", width: 100, height: 100 }
+            },
+            opacity: {
+              value: 0.5,
+              random: true,
+              anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false }
+            },
+            size: {
+              value: 95,
+              random: true,
+              anim: {
+                enable: true,
+                speed: 0,
+                size_min: 0,
+                sync: false
+              }
+            },
+            line_linked: {
+              enable: false,
+              distance: 978.5430756240181,
+              color: "#ffffff",
+              opacity: 0.3,
+              width: 2.8409315098761816
+            },
+            move: {
+              enable: true,
+              speed: 20,
+              direction: "none",
+              random: true,
+              straight: false,
+              out_mode: "out",
+              bounce: false,
+              attract: { enable: true, rotateX: 600, rotateY: 1183.721462448409 }
+            }
+          },
+          interactivity: {
+            detect_on: "canvas",
+            events: {
+              onhover: { enable: false, mode: "grab" },
+              onclick: { enable: false, mode: "push" },
+              resize: true
+            },
+            modes: {
+              grab: { distance: 400, line_linked: { opacity: 1 } },
+              bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
+              repulse: { distance: 200, duration: 0.4 },
+              push: { particles_nb: 4 },
+              remove: { particles_nb: 2 }
+            }
+          },
+          retina_detect: true
+        });
+        requestAnimationFrame(update); 
+
+      } 
+
+    });
 	</script>
 </body>
 
