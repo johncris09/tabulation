6be88ab62e05e59940d62ab3077290c619e7c27b
John Cris <johncrismanabo9@gmail.com>
2022-10-08 16:19:05 +0800

    production number and attire scoring

--- application/views/user/production_number.php
+++ application/views/user/production_number.php
@@ -4,6 +4,11 @@
 <head>
 	<?php $this->view('layout/meta') ?>
 	<?php $this->view('layout/css') ?> 
+  <style>
+    .seperate{
+      border-left: 5px solid gray !important;
+    }
+  </style>
 </head>
 
 <body> 
@@ -21,15 +26,30 @@
             <div class="card"> 
               <div class="card-header d-flex justify-content-between pb-3">
                 <div class="conversion-title">
-                  <h5 class="card-title mb-1"> <?php echo $page_title; ?> </h5> 
-									<p><span class="text-danger font-weight-bold">Criteria:</span> Each candidate will be rated 1 to 10, 1 being the lowest and 10 being the highest based on
-										<strong>Mastery, Gracefulness and Stage projection.</strong> 
-									</p>
+                  <h5 class="card-title mb-1"> <?php echo $page_title; ?> </h5>   
                 </div>
 								<div>
-                	<button class="btn btn-primary" <?php echo $status == "locked" ? "disabled" : "" ?> id="submit-score"> <i class=" bx bx-check"> </i> Submit Score</button>
+                	<button class="btn btn-primary" <?php echo $status == "locked" ? "disabled" : "" ?> id="submit-score"> <i class=" bx bx-check"> </i> Submit Production Scores</button>
 								</div>
               </div>
+              <div class="card-body">
+                <table class="table "> 
+                  <tr class="text-center">
+                    <th colspan="2" class="text-danger">Criteria</th>
+                  </tr> 
+                  <tr>
+                    <th>Production Number</th>
+                    <td>Each candidate will be rated 1 to 10, 1 being the lowest and 10 being the highest based on
+                <strong>Mastery, Gracefulness and Stage projection.</strong></td>
+                  </tr> 
+                  <tr>
+                    <th>Production Attire</th>
+                    <td>Each candidate will be rated 1 to 10, 1 being the lowest and 10 being the highest based on
+                <strong>Attire to candidate's match, Poise and carriage and General beauty.</strong></td>
+                  </tr>  
+              </table> 
+
+              <hr style="border-top:2px dotted #000;">
               <div class="table-responsive text-nowrap">
                 <table class="table table-striped table-bordered">
                   <thead>
@@ -37,7 +57,7 @@
                       <th> </th> 
                       <th colspan="2">Production Number</th>
                       <th colspan="2">Production Attire</th>
-                      <th colspan="2">Top Five</th> 
+                      <th class="seperate" colspan="2">Top Five</th> 
                     </tr>
                     <tr class="text-center"> 
                       <th>Candidate</th> 
@@ -45,7 +65,7 @@
                       <th>Rank</th> 
                       <th>Score</th>
                       <th>Rank</th> 
-                      <th>Score</th>
+                      <th class="seperate" >Score</th>
                       <th>Rank</th> 
                     </tr>
                   </thead>
@@ -57,16 +77,16 @@
                           echo '
                             <tr class="text-center"> 
                               <td class=""> <div class="star">#' .$row['number']. '</div>     </td> 
-                              <td><input '.$readonly.'  type="number" data-candidate="'.$row['id'].'"  step="0.01" min="1" max="10"  class="form-control text-center candidate"  ></td>
-                              <td> <span data-candidate="'.$row['id'].'" class="rank h6 text-center candidate-'.$row['id'].'"></span> </td> 
+                              <td><input '.$readonly.' id="score-production-number" type="number" data-candidate="'.$row['id'].'"  step="0.01" min="1" max="10"  class="form-control text-center candidate"  ></td>
+                              <td> <span data-candidate="'.$row['id'].'" class="rank-production-number h6 text-center candidate-'.$row['id'].'"></span> </td> 
 
                               
-                              <td><input '.$readonly.'  type="number" data-candidate="'.$row['id'].'"  step="0.01" min="1" max="10"  class="form-control text-center candidate"  ></td>
-                              <td> <span data-candidate="'.$row['id'].'" class="rank h6 text-center candidate-'.$row['id'].'"></span> </td> 
+                              <td><input '.$readonly.'  id="score-production-attire"   type="number" data-candidate="'.$row['id'].'"  step="0.01" min="1" max="10"  class="form-control text-center candidate"  ></td>
+                              <td> <span data-candidate="'.$row['id'].'" class="rank-production-attire h6 text-center candidate-'.$row['id'].'"></span> </td> 
 
                               
-                              <td><input '.$readonly.'  type="number" data-candidate="'.$row['id'].'"  step="0.01" min="1" max="10"  class="form-control text-center candidate"  ></td>
-                              <td> <span data-candidate="'.$row['id'].'" class="rank h6 text-center candidate-'.$row['id'].'"></span> </td> 
+                              <td class="seperate" ><input '.$readonly.'   id="score-top-five"   type="number" data-candidate="'.$row['id'].'"  step="0.01" min="1" max="10"  class="form-control text-center candidate"  ></td>
+                              <td> <span data-candidate="'.$row['id'].'" class="rank-top-five h6 text-center candidate-'.$row['id'].'"></span> </td> 
                             </tr> 
                           ';
                         } 
@@ -75,6 +95,12 @@
                   </tbody>
                 </table>
               </div>
+
+
+              </div>
+              
+
+            
             </div> 
             
 
@@ -90,39 +116,43 @@
 	<div class="layout-overlay layout-menu-toggle"></div> 
 	<div class="drag-target"></div> 
 	<?php $this->view('layout/js') ?>
-  <script>
-    // window.onload = function () {
-    //   load_rank();
-    // }
+  <script> 
     $(document).ready(function(){  
-      load_score(); 
-      load_rank();
+      load_production_number_score(); 
+      load_production_number_rank();
+      load_production_attire_score(); 
+      load_production_attire_rank();
       
 
-    function load_rank(){
- 
-      $.ajax({
-        url: BASE_URL + 'production_number/get_ranking_specific_judge',
-        type: 'POST',   
-        dataType: 'JSON',
-        success: function(data){  
-          // console.info(data)
-          $.each(data, function (key, val) {  
-            $('span.candidate-' + val.candidate).html(val.rank) 
-          });
-          
-        }, 
-        error: function(xhr, textStatus, error){
-          console.info(xhr.responseText);
-        }
-      }); 
+      //________________________________//
+      //________________________________//
+      //        Production Number
+      //________________________________//
+      //________________________________//
+      function load_production_number_rank(){
+  
+        $.ajax({
+          url: BASE_URL + 'production_number/get_ranking_specific_judge',
+          type: 'POST',   
+          dataType: 'JSON',
+          success: function(data){  
+            // console.info(data)
+            $.each(data, function (key, val) {  
+              $('span.rank-production-number.candidate-' + val.candidate).html(val.rank) 
+            });
+            
+          }, 
+          error: function(xhr, textStatus, error){
+            console.info(xhr.responseText);
+          }
+        }); 
 
-    }
+      } 
 
       // load score
-    function load_score()
+      function load_production_number_score()
       {
-        $('input.candidate').each(function(){   
+        $('input#score-production-number.candidate').each(function(){   
           var _this = $(this)
             $.ajax({
               type : 'POST',
@@ -137,13 +167,13 @@
               }
             }); 
         })
-      }
+      } 
  
 			
       // save score while typing
       // 1 to 10 input only
 
-      $('input').on('keyup', function(){  
+      $('input#score-production-number').on('keyup', function(){  
         var _this = this
         var candidate = $(this).data('candidate') 
  
@@ -155,7 +185,7 @@
           }).then(function(){
             // empty all fields
             _this.value = "";  
-            $('.rank.candidate-' + candidate).html('')
+            $('.rank-production-number.candidate-' + candidate).html('')
           }) 
 					 
 
@@ -169,13 +199,13 @@
             },
             dataType: "json",
             success : function(data){    
-              load_rank();
+              load_production_number_rank();
             }
           });  
 
         }else if(_this.value  == 0  ){    
 					_this.value = "";  
-            $('.rank.candidate-' + candidate).html('') 
+            $('.rank-production-number.candidate-' + candidate).html('') 
 					// delete previous score by candidate and judge
 					$.ajax({
             type : 'POST',
@@ -186,12 +216,12 @@
             },
             dataType: "json",
             success : function(data){    
-              load_rank();
+              load_production_number_rank();
             }
           }); 
         }else if(_this.value  == ""   ){  
           _this.value = "";  
-          $('.rank.candidate-' + candidate).html('')  
+          $('.rank-production-number.candidate-' + candidate).html('')  
 					// delete previous score by candidate and judge
 					$.ajax({
             type : 'POST',
@@ -202,7 +232,7 @@
             },
             dataType: "json",
             success : function(data){    
-              load_rank();
+              load_production_number_rank();
             }
           }); 
         }else{  
@@ -218,21 +248,164 @@
             },
             dataType: "json",
             success : function(data){   
-              if(data.response == true){ 
+              // if(data.response == true){ 
                 
-                $(_this).css("border", "1px solid blue");
-                $(_this).css('font-weight', 'bolder');
-                setTimeout(function() {  
-                  $(_this).css("border", "1px solid black");
-                  $(_this).css('font-weight', 'normal');
-                }, 500); 
+              //   $(_this).css("border", "1px solid blue");
+              //   $(_this).css('font-weight', 'bolder');
+              //   setTimeout(function() {  
+              //     $(_this).css("border", "1px solid black");
+              //     $(_this).css('font-weight', 'normal');
+              //   }, 500); 
                 
-              } 
-              load_rank();
+              // } 
+              load_production_number_rank();
             }
           }); 
         } 
       });
+
+
+      
+
+      //________________________________//
+      //________________________________//
+      //        Production Attire
+      //________________________________//
+      //________________________________//  
+      function load_production_attire_rank(){
+        
+        $.ajax({
+          url: BASE_URL + 'production_attire/get_ranking_specific_judge',
+          type: 'POST',   
+          dataType: 'JSON',
+          success: function(data){  
+            // console.info(data)
+            $.each(data, function (key, val) {  
+              $('span.rank-production-attire.candidate-' + val.candidate).html(val.rank) 
+            });
+            
+          }, 
+          error: function(xhr, textStatus, error){
+            console.info(xhr.responseText);
+          }
+        }); 
+
+      }
+
+      
+      // load score
+      function load_production_attire_score()
+      {
+        $('input#score-production-attire.candidate').each(function(){   
+          var _this = $(this)
+            $.ajax({
+              type : 'POST',
+              url : BASE_URL + "production_attire/get_candidate_score",
+              data : { 
+                candidate : $(this).data('candidate'),
+                judge : '<?php echo $_SESSION['id'] ?>', 
+              },
+              dataType: "json",
+              success : function(data){   
+                $(_this).val(data)
+              }
+            }); 
+        })
+      }
+
+
+      $('input#score-production-attire').on('keyup', function(){  
+        var _this = this
+        var candidate = $(this).data('candidate') 
+ 
+ 
+        if(_this.value > 10  ){ 
+          Swal.fire({
+            icon: 'error',
+            title: 'Please only provide ratings between 1 and 10.', 
+          }).then(function(){
+            // empty all fields
+            _this.value = "";  
+            $('.rank-production-attire.candidate-' + candidate).html('')
+          }) 
+					 
+
+					// delete previous score by candidate and judge
+					$.ajax({
+            type : 'POST',
+            url : BASE_URL + "production_attire/delete_previous_score", 
+            data : { 
+              candidate : $(this).data('candidate'),
+              judge : '<?php echo $_SESSION['id'] ?>', 
+            },
+            dataType: "json",
+            success : function(data){    
+              load_production_attire_rank();
+            }
+          });  
+
+        }else if(_this.value  == 0  ){    
+					_this.value = "";  
+            $('.rank-production-attire.candidate-' + candidate).html('') 
+					// delete previous score by candidate and judge
+					$.ajax({
+            type : 'POST',
+            url : BASE_URL + "production_attire/delete_previous_score", 
+            data : { 
+              candidate : $(this).data('candidate'),
+              judge : '<?php echo $_SESSION['id'] ?>', 
+            },
+            dataType: "json",
+            success : function(data){    
+              load_production_attire_rank();
+            }
+          }); 
+        }else if(_this.value  == ""   ){  
+          _this.value = "";  
+          $('.rank-production-attire.candidate-' + candidate).html('')  
+					// delete previous score by candidate and judge
+					$.ajax({
+            type : 'POST',
+            url : BASE_URL + "production_attire/delete_previous_score", 
+            data : { 
+              candidate : $(this).data('candidate'),
+              judge : '<?php echo $_SESSION['id'] ?>', 
+            },
+            dataType: "json",
+            success : function(data){    
+              load_production_attire_rank();
+            }
+          }); 
+        }else{  
+          var _this = $(this)  
+          $.ajax({
+            type : 'POST',
+            url : BASE_URL + "production_attire/insert", 
+            data : {
+              score : $(this).val(),
+              candidate : $(this).data('candidate'),
+              judge : '<?php echo $_SESSION['id'] ?>',
+
+            },
+            dataType: "json",
+            success : function(data){   
+              // if(data.response == true){ 
+                
+              //   $(_this).css("border", "1px solid blue");
+              //   $(_this).css('font-weight', 'bolder');
+              //   setTimeout(function() {  
+              //     $(_this).css("border", "1px solid black");
+              //     $(_this).css('font-weight', 'normal');
+              //   }, 500); 
+                
+              // } 
+              load_production_attire_rank();
+            }
+          }); 
+        } 
+      });
+
+
 
 			
       // submit score 
