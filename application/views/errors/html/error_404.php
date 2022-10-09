bda777cba0f21fdaee91a6ecdedf6d276c5d080c
John Cris <johncrismanabo9@gmail.com>
2022-10-09 22:25:17 +0800

    change page title

--- application/views/errors/html/error_404.php
+++ application/views/errors/html/error_404.php
@@ -9,7 +9,7 @@ $base_url = load_class('Config')->config['base_url'];
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Bootstrap 5 404 page with image</title>
+        <title>404 Page not found</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     </head>
 
