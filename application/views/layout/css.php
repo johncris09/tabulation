5cd003815d2da0301fe0b126a9f462bd01dfa022
John Cris <johncrismanabo9@gmail.com>
2022-10-18 09:32:17 +0800

    offline fonts

--- application/views/layout/css.php
+++ application/views/layout/css.php
@@ -5,8 +5,8 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
-
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/font.css" />
+    
     <!-- Icons -->
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/vendor/fonts/boxicons/css/boxicons.min.css" />
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/vendor/fonts/fontawesome.css" />
